fc6a9c6be1289a6d146631edc6552ca36658327f
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-11-26 01:33:08 -0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -45,7 +45,7 @@ module.exports = {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     network_id: "12345",       // Any network (default: none)
      //from: "0x3Fbc18C02ee3A8D787cbe12725Fd997d3522fc7e"
     },
 
